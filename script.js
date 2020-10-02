73871860858b19caa8f2ca03cd8760e528d882ce
Beth Presten <bethpresten@gmail.com>
2020-10-02 18:53:51 -0400

    copied image for reference, readme instructions, added time-block, hour, and button divs to html, added rowEl with time populating in each row, added current date

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
     console.log("This is loading!")
     // dom variables
+    var timeBlockContainer = $(".time-block");
+    var workHours = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00pm", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
+    var currentHour = moment().format("H");
 
 
-    // js variables
+    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+    // for loop to create the row time blocks
+    for (var i = 0; i < workHours.length; i++) {
+        // js variables
+        var rowTimeEl = $("<div>").addClass("row time-block");
+        var rowHourEl = $("<div>").addClass("hour col-sm-1").attr("id", workHours[i]);
+        var textInput = $("<textarea>").addClass("col-sm-10");
+        var saveBtn = $("#saveBtn");
+        rowHourEl.text(workHours[i]);
+
+
+
+
+
+        rowTimeEl.append(rowHourEl, textInput);
+        timeBlockContainer.append(rowTimeEl);
+    }
 
     //function code
 
