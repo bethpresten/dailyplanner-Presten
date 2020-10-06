0cb4b3a63ade2e6d68e1272ab28931ee971bf2f7
Beth Presten <bethpresten@gmail.com>
2020-10-06 15:19:45 -0400

    added screenshot of working product, added comments to index and script pages, added save icon to the save button

--- README.md
+++ README.md
@@ -1,89 +1,13 @@
-# dailyplanner-Presten
+Work Day Scheduler
 
-# 05 Third-Party APIs: Work Day Scheduler
+Screenshot:
+![Image of final product][./assets/workday.png]
 
-## Your Task
+Use:
+A user has the ability to schedule or log the events of his/her work day. Time is color-coded based on whether it's in the past, present, or future.
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Installation:
+I built this application based on prior code that was provided by Trilogy. I hard-coded as few items as possible in HTML and attempted to dynamically create items within JQuery to practice those coding skills. The CSS elements provided were unaltered and were also helpful in building this application. In JQuery, I began the process with DOM elements and then went on to JS elements and then the event listener. After that, I concentrated on the moment.js and then data persistance and local storage.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```
-
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
-WHEN I click into a time block
-THEN I can enter an event
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria:
-
-### Technical Acceptance Criteria: 40%
-
-- Satisfies all of the above acceptance criteria plus the following:
-
-  - Uses the Moment.js library to work with date and time
-
-### Deployment: 32%
-
-- Application deployed at live URL
-
-- Application loads with no errors
-
-- Application GitHub URL submitted
-
-- GitHub repo contains application code
-
-### Application Quality: 15%
-
-- Application user experience is intuitive and easy to navigate
-
-- Application user interface style is clean and polished
-
-- Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-- Repository has a unique name
-
-- Repository follows best practices for file structure and naming conventions
-
-- Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-- Repository contains multiple descriptive commit messages
-
-- Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-- The URL of the functional, deployed application.
-
-- The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
----
-
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+Credits:
+My classmates, BCS Bootcamp Spots, and my T.A. for helping me through this process.
--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,28 @@
 $(document).ready(function () {
     // console.log("This is loading!")
-    // dom variables
+    // DOM variables
     var timeBlockContainer = $(".time-block");
     var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
     var workHours = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
     var currentHour = moment().format("HH");
-    console.log(currentHour);
+    // Checking to make sure the moment.js was pulled correctly
+    // console.log(currentHour);
 
+    // Format for the date at the top of the page located within the jumbotron
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
     // for loop to create the row time blocks
-
     for (var i = 0; i < workHours.length; i++) {
-        // js variables
+        // js variables (3 step process)
         var rowTimeEl = $("<div>").addClass("row time-block");
         var rowHourEl = $("<div>").addClass("hour col-sm-1").attr("id", "H" + workHours[i].replace(" ", "").replace(":", ""));
-        var textInput = $("<input>").addClass("col-sm-10");
-
+        var textInput = $("<textarea>").addClass("col-sm-10").addClass("textarea");
         rowTimeEl.append(rowHourEl, textInput);
         timeBlockContainer.append(rowTimeEl);
-
+        // Adding in each hour to every row
         rowHourEl.text(workHours[i]);
         // console.log(workHours[i].split(":")[0], "workHour");
+        // Conditional statement to color-code time events
         if (militaryHours[i] < currentHour) {
             textInput.addClass("past");
         }
@@ -32,13 +33,15 @@ $(document).ready(function () {
             textInput.addClass("present");
         }
 
-
-        var saveBtn = $("<button>").attr("class", "col-sm-1").text("save");
+        // adding in the save icon image to the save button as well as color-coding it from CSS
+        // 3 step process for creating the button
+        var saveBtn = $("<button>").attr("class", "col-sm-1").addClass("i fas fa-save").addClass("saveBtn");
+        saveBtn.attr("src", "assets/save-icon.png");
         rowTimeEl.append(saveBtn);
-        var savePic = $("<image>").attr("src", "./assets/save-icon.png");
 
+        // Event Listener for save button
         saveBtn.on("click", function () {
-            var savedSchedule = ($(this).siblings("input").val());
+            var savedSchedule = ($(this).siblings("textarea").val());
             var anotherSavedItem = ($(this).siblings(".hour").attr("id")).replace(" ", "").replace(":", "");
             localStorage.setItem(anotherSavedItem, savedSchedule);
             console.log(saveBtn);
@@ -48,19 +51,16 @@ $(document).ready(function () {
         })
     }
 
-
-    $("#H900AM").siblings("input").val(localStorage.getItem("H900AM"));
-    $("#H1000AM").siblings("input").val(localStorage.getItem("H1000AM"));
-    $("#H1100AM").siblings("input").val(localStorage.getItem("H1100AM"));
-    $("#H1200PM").siblings("input").val(localStorage.getItem("H1200PM"));
-    $("#H100PM").siblings("input").val(localStorage.getItem("H100PM"));
-    $("#H200PM").siblings("input").val(localStorage.getItem("H200PM"));
-    $("#H300PM").siblings("input").val(localStorage.getItem("H300PM"));
-    $("#H400PM").siblings("input").val(localStorage.getItem("H400PM"));
-    $("#H500PM").siblings("input").val(localStorage.getItem("H500PM"));
-    //function code
-
-    //event listeners
+    // data persistence for all of the text areas
+    $("#H900AM").siblings("textarea").val(localStorage.getItem("H900AM"));
+    $("#H1000AM").siblings("textarea").val(localStorage.getItem("H1000AM"));
+    $("#H1100AM").siblings("textarea").val(localStorage.getItem("H1100AM"));
+    $("#H1200PM").siblings("textarea").val(localStorage.getItem("H1200PM"));
+    $("#H100PM").siblings("textarea").val(localStorage.getItem("H100PM"));
+    $("#H200PM").siblings("textarea").val(localStorage.getItem("H200PM"));
+    $("#H300PM").siblings("textarea").val(localStorage.getItem("H300PM"));
+    $("#H400PM").siblings("textarea").val(localStorage.getItem("H400PM"));
+    $("#H500PM").siblings("textarea").val(localStorage.getItem("H500PM"));
 });
 
 
